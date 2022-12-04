6af2fde9e875be1680e129d72af9318619ed9e09
ChaotenHG <chaoten@neverstopgaming.net>
2022-12-04 13:15:37 +0100

    Day4

--- src/Day04.kt
+++ src/Day04.kt
@@ -2,19 +2,44 @@
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+
+        var overlaping = 0
+
+        input.forEach { line ->
+
+            val (elfOne, elfTwo) = line.split(",").map { it.split("-") }.map { it -> it.map { it.toInt() } }
+
+            val (elfOneStart, elfOneEnd) = elfOne
+            val (elfTwoStart, elfTwoEnd) = elfTwo
+
+            if(elfOneStart >= elfTwoStart && elfOneEnd <= elfTwoEnd || elfTwoStart >= elfOneStart && elfTwoEnd <= elfOneEnd ) {
+               overlaping++
+            }
+        }
+
+        return overlaping
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+
+        var overlaping = 0
+
+        input.forEach { line ->
+
+            val (elfOne, elfTwo) = line.split(",").map { it.split("-") }.map { it -> it[0].toInt()..it[1].toInt() }
+
+            overlaping += if(elfOne.any { it in elfTwo }) 1 else 0
+        }
+
+        return overlaping
     }
 
-    val testInput = readTestInput("02")
-    check(part1(testInput) == 0)
+    val testInput = readTestInput("04")
+    check(part1(testInput) == 2)
 
-    val input = readInput("02")
+    val input = readInput("04")
     println("Part 1: ${part1(input)}")
 
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 4)
     println("Part 2: ${part2(input)}")
 }
